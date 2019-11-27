b540682365331863cb9f00e855d8000163e84f47
pkro <pkrombach@web.de>
2019-11-27 08:37:50 +0100

    added function to read text from url

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ const main = () => {
     preview.innerHTML = marked(editor.value);
 }
 
+const readTextURL = url => {
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.send(null);
+    request.onreadystatechange = () => {
+        var type = request.getResponseHeader('Content-Type');
+            if (type.indexOf("text") !== 1) {
+                return request.responseText;
+            }
+    }
+}
+
 document.addEventListener('DOMContentLoaded', main);
